52e8e8060545a88eab96b28822d0c277315620cb
Derik <dpack@ieee.org>
2022-11-27 23:42:55 -0500

    Version .9 of my presentation for the Citadel on Python

---
+++ a_python_file.py
@@ -0,0 +1,13 @@
+print("hello world")
+print(
+    "THis is an ugly thing"
+    "Another"
+    "Another"
+    "Another"
+    "Another"
+    "Another"
+    "Another"
+    "Another"
+    "Another"
+)
+print("hi, my name is.")
